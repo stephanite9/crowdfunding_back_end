3528745d6f598e21143579d2abaa45646a06f24b
Stephanie Chan <stephanite.chan@gmail.com>
2025-08-16 12:58:45 +0800

    Added users app and updated setting.py

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'fundraisers.apps.FundraisersConfig',
+    'users.apps.UsersConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
