b045d8fa3d4593970bcaf8e7f859e5c72994ee1f
Stephanie Chan <stephanite.chan@gmail.com>
2025-08-19 19:58:11 +0800

    migrated stuff

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Fundraiser(models.Model):
     title = models.CharField(max_length=200)
@@ -7,13 +8,23 @@ class Fundraiser(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
-    
+    owner = models.ForeignKey(
+        get_user_model(),
+        related_name='owned_fundraisers',
+        on_delete=models.CASCADE,
+    )
+
 class Pledge(models.Model):
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
     anonymous = models.BooleanField()
     fundraiser = models.ForeignKey(
         'Fundraiser',
+        related_name='pledges',
+        on_delete=models.CASCADE,
+    )
+    supporter = models.ForeignKey(
+        get_user_model(),
         related_name='pledges',
         on_delete=models.CASCADE,
     )
