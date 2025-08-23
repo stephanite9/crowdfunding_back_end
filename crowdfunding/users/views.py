fc4064611083899dd0f283d7f7cd17b76b629820
Stephanie Chan <stephanite.chan@gmail.com>
2025-08-23 12:47:33 +0800

    Added token authentication

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from django.http import Http404
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 class CustomUserList(APIView):
     def get(self, request):
@@ -37,3 +39,20 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data = request.data,
+            context = {'request': request},
+        )
+        serializer.is_valid(raise_exception = True)
+        user = serializer.validated_data['user']
+
+        token, created = Token.objects.get_or_create(user = user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email,
+        })
